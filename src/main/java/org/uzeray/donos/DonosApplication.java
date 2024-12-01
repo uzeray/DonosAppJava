f5f52a69cab8f1ebe1443472ee69aeadd52db108
Unal Zeray <uzeray@gmail.com>
2024-12-02 00:54:56 +0100

    created project and get ready to build

---
+++ src/main/java/org/uzeray/donos/DonosApplication.java
@@ -0,0 +1,13 @@
+package org.uzeray.donos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonosApplication.class, args);
+    }
+
+}
